908c7f214a1a560ac983f624550722923ff8e487
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-12-02 12:27:27 +0530

    2 videos completed

--- main.py
+++ main.py
@@ -23,13 +23,17 @@ async def create_book(book_data: BookCreateModel):
         "author": book_data.author
     }
 
-@app.get('/get_headers')
+@app.get('/get_headers', status_code=201)
 async def get_headers(accept:str=Header(None),
-                      content_type:str=Header(None)): 
+                      content_type:str=Header(None),
+                      user_agent:str=Header(None),
+                      host:str=Header(None)): 
     
     request_headers = {}
 
     request_headers['Accept'] = accept
     request_headers['Content-Type'] = content_type
+    request_headers['User-Agent'] = user_agent
+    request_headers['Host'] = host
     
     return request_headers
